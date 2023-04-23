6fff17f513dbea30d6ae0739386d99eb368d1761
hakseon0309 <120485392+hakseon0309@users.noreply.github.com>
2023-04-24 04:03:27 +0900

    230424

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css"
+import Aberrus from "./component/wow/aberrus/Aberrus";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <div className="container">
+        <Aside/>
+        <Post/>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
+function Header() {
+  const [header, setHeader] = useState("HAKSEON LAB - WLB 10.1");
+  return (
+      <header>
+          { header }
+      </header>
+  );
+};
+function Aside() {
+  const [banner, setBanner] = useState("https://media.tenor.com/iixn8CyQDywAAAAd/blue-archive.gif");
+  const [icon, setIcon] = useState("https://wow.zamimg.com/images/wow/icons/large/inv_pet_cats_siamesecat.jpg");
+  const [character, setCharacter] = useState("학선양");
+  const [server, setServer] = useState("KR-Azshara");
+  return (
+      <aside>
+          <img className="banner" src= { banner } alt=""/>
+          <div className="profile">
+              <div className="background">
+                  <img className="icon" src= { icon } alt=""/>
+                  <div>
+                      <div className="character monk">
+                          { character }
+                      </div>
+                      <div className="server">
+                          { server }
+                      </div>
+                  </div>    
+              </div>
+          </div>
+      </aside>
+  );
+};
+function Post() {
+  const [title, setTitle] = useState("[10.1] 어둠의 도가니, 아베루스")
+  const [content, setContent] = useState([<Aberrus named="0"/>])
+  return (
+  <div className="post">
+      <div className="title">
+          { title }
+      </div>
+      <div className="content">
+          { content }
+      </div>
+  </div>
+  )
+};
---
+++ src/component/wow/aberrus/Aberrus.js
@@ -0,0 +1,134 @@
+import React from 'react'
+import "./Aberrus.css"
+
+function Aberrus(props) {
+
+    let index = parseInt(props.named);
+    let indexNext = index + 1;
+
+    let bossIcon = [
+        "img/wow/aberrus/18-03-23-10-kazzara-the-hellforged.webp",
+        "img/wow/aberrus/20-03-23-0-the-amalgamation-chamber.webp",
+        "img/wow/aberrus/18-03-23-9-the-forgotten-experiments.jpg",
+        "img/wow/aberrus/18-03-23-7-assault-of-the-zaqali.webp",
+        "img/wow/aberrus/18-03-23-8-rashok-the-elder.jpg",
+        "img/wow/aberrus/18-03-23-2-the-vigilant-steward-zskarn.webp",
+        "img/wow/aberrus/18-03-23-1-magmorax.webp",
+        "img/wow/aberrus/18-03-23-7-echo-of-neltharion.webp",
+        "img/wow/aberrus/18-03-23-6-Scalecommander Sarkareth.webp",
+    ]
+
+    let bossImg = [
+        "img/wow/aberrus/kazzarathehellforged.webp",
+        "img/wow/aberrus/theamalgamationchamber.png",
+        "img/wow/aberrus/theforgottenexperiments.webp",
+        "img/wow/aberrus/assaultofthezaqali.webp",
+        "img/wow/aberrus/rashoktheelder.webp",
+        "img/wow/aberrus/thevigilantstewardzskarn.webp",
+        "img/wow/aberrus/magmorax.webp",
+        "",
+        "",
+    ]
+
+    let tankIcon = "img/wow/icon/tank-icon.webp";
+    let healerIcon = "img/wow/icon/healer-icon.webp";
+    let dpsIcon = "img/wow/icon/dps-icon.webp";
+    
+    return (
+    <div className="aberrus">
+
+        <div className="nav">
+            <img className="bossIcon" src={bossIcon[0]} alt=""/>
+            <img className="bossIcon" src={bossIcon[1]} alt="" style={{opacity: 0.3}}/>
+            <img className="bossIcon" src={bossIcon[2]} alt="" style={{opacity: 0.3}}/>
+            <img className="bossIcon" src={bossIcon[3]} alt="" style={{opacity: 0.3}}/>
+            <img className="bossIcon" src={bossIcon[4]} alt="" style={{opacity: 0.3}}/>
+            <img className="bossIcon" src={bossIcon[5]} alt="" style={{opacity: 0.3}}/>
+            <img className="bossIcon" src={bossIcon[6]} alt="" style={{opacity: 0.3}}/>
+            <img className="bossIcon" src={bossIcon[7]} alt="" style={{opacity: 0.3}}/>
+            <img className="bossIcon" src={bossIcon[8]} alt="" style={{opacity: 0.3}}/>
+        </div>
+
+        <div className="scrollspy">
+            <div>인원 구성</div>
+            <div>능력 숙지</div>
+            <div>공략 준비</div>
+        </div>
+
+        <div className="bossTitle">
+            <div className="text">
+                <div className="raid">어둠의 도가니, 아베루스</div>
+                <div className="boss">지옥벼림 카자라</div>
+                <div className="sub">영웅 난이도 공략</div>
+            </div>
+            <div className='img'>
+                <img src={bossImg[index]} alt=""/>
+            </div>
+        </div>
+
+        <div className="difficulty">
+            <div className="heroic">HEROIC</div>
+            <div className="mythic">MYTHIC</div>
+        </div>
+
+        <div className="info">마지막 수정: 2023 / 4 / 23</div>
+
+        <div className="raidSetup">
+            RAID SETUP
+            <div className="positionIcon">
+                <img src={tankIcon} alt=""/>2
+                <img src={healerIcon} alt=""/>4
+                <img src={dpsIcon} alt=""/>14+
+            </div>
+        </div>
+
+        <div className="introduction">INTRODUCTION</div>
+
+        <div className="introductionContent">
+            <strong>Fight Style</strong>: Single Target Fight<br/>
+            <strong>Recommended Setup</strong>: 2 Tanks / 4 Healers / 14+ DPS<br/>
+            <strong>Useful utilities</strong>: A second monitor to watch YouTube during the boss fight<br/>
+        </div>
+
+        <div className="mechanics">MECHANICS</div>
+
+        <div className="mechanicsContent">
+        </div>
+
+        <div className="strategy">STRATEGY</div>
+
+        <div className="strategyContent">
+            <div className="stConTitle">OPENING POSITION</div>        
+            <div className="img"><img src="https://www.method.gg/images/guides/uploads/kazzara-the-hellforged-opening-position.jpg" alt=""></img></div>
+            <div className="paragraph">
+                <p>
+                    Tanks should keep the boss where she is standing and face her away from the raid. Even though she does no frontal attack, facing her away will help with awareness when the boss uses her Hellbeam ability (she will do a 180 turn). Tank swap each Terror Claws unless you want to test your limits.
+
+                    Pop Hero/Lust on pull, and loosely spread behind the boss to not get caught by random lasers spawning on nearby players and to bait other abilities. The boss will reach 70% fairly quickly, so be ready to dodge puddles and to take raid wide damage.
+
+                    The puddles she spawns when she hits 70%, 50% and 30% hit pretty hard, so avoid at all cost.
+
+                    The Hellsteel Fragments only spawn at those breakpoints, but Revenant's Blood puddles spawn throughout the fight at increasing intensity the lower the boss' health is.
+
+                    When the lasers spawn, kite them into the Dread Rifts puddles. The placement of Dread Rifts does not matter much as there is plenty of space behind the boss (just don’t place them too far away, as that will force players to kite for longer).
+
+                    At 100 energy, be ready to dodge the frontal breath ability (Hellbeam). She will lock her position once she starts casting, so just dodge once.
+
+                    The Wings of Extinction cast will do a small knockback, so just try not to get knocked into a laser/puddle, to avoid taking unnecessary damage. 
+
+                    When the boss is below 30% health, the raid-wide damage will ramp up pretty high, so be extra careful to avoid taking unnecessary damage from other abilities, as this will most likely lead to deaths at this point.
+                </p>
+            </div>
+        </div>
+        <div className="nextBoss">
+            <div className='text'>
+                <div className='next'>NEXT BOSS</div>
+                <div className='name'>융합체의 방</div>
+            </div>            
+            <div className='img'><img src={bossImg[indexNext]} alt=""/></div>
+        </div>
+    </div>
+    )
+}
+
+export default Aberrus
